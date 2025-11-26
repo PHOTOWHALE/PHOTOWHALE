49d341f10dbf08d5c986ca6dc5e313329ebf0a36
LeeYunHwan <cyzhqly@gmail.com>
2025-11-26 16:05:24 +0900

    setting: Lefthook 설치 및 pre-commit 훅 연결 (#1)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 
-import { defineConfig, globalIgnores } from "eslint/config";
-import storybook from "eslint-plugin-storybook";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import storybook from 'eslint-plugin-storybook';
 
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 export default defineConfig([
   // --- Next.js 기본 recommended + TS + Core Web Vitals ---
@@ -23,22 +21,6 @@ export default defineConfig([
     },
   },
 
-  // --- Prettier 연동 ---
-  // eslint-config-prettier -> ESLint formatting rule들을 끔
-  // eslint-plugin-prettier -> ESLint 오류로 Prettier 포맷 경고를 띄움
-  {
-    ...prettierConfig,
-    plugins: { prettier },
-    rules: {
-      "prettier/prettier": "error",
-    },
-  },
-
   // --- Next 기본 ignore 수정(프로젝트 상황에 맞게 유지) ---
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
