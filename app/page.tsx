792bbcd66633b2e6ed069a25fb274546abc3045b
Sonseongoh <nobleser@naver.com>
2025-12-01 01:54:22 +0900

    feat: 버튼 컴포넌트 (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Button from '@/component/ui/Button';
+
 export default function Home() {
   return (
     <>
@@ -9,6 +11,14 @@ export default function Home() {
       <div>Time-Film</div>
       <div>Time-Film</div>
       <div>Time-Film</div>
+      <Button>기본 버튼</Button>
+      <br />
+      <Button variant="secondary">서브 버튼</Button>
+      <br />
+      <Button full>가로 100% 버튼</Button>
+      <br />
+      <Button disabled>비활성화</Button>
+      <br />
     </>
   );
 }
