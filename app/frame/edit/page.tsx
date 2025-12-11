9af20a775d2a6917d6bd09f5ecdd3c45b6b671da
LeeYunHwan <cyzhqly@gmail.com>
2025-12-11 23:30:21 +0900

    feat: 프레임 스킨 store 구현 및 관련 파일 작성 (#15)

--- app/frame/edit/page.tsx
+++ app/frame/edit/page.tsx
@@ -2,38 +2,57 @@
 
 import PhotoFrame from '@/components/common/PhotoFrame';
 import { useFrameStore } from '@/stores/useFrameStore';
+import useSkinStore from '@/stores/useSkinStore';
 import { useProgressStore } from '@/stores/useProgressStore';
 import { useEffect, useRef } from 'react';
 import SwiperType from 'swiper';
 import Carousel from '@/components/common/Carousel';
 import { SwiperSlide } from 'swiper/react';
 import { COLORS } from '@/types/colors';
+import { SKINS } from '@/types/skins';
+import Image from 'next/image';
+
+type BtnClickEventType = 'skin' | 'color';
 
 export default function FrameEditPage() {
-  const swiperRef = useRef<SwiperType | null>(null);
+  const swiperColorRef = useRef<SwiperType | null>(null);
+  const swiperSkinRef = useRef<SwiperType | null>(null);
+
   const bgColor = useFrameStore(s => s.color);
   const setbgColor = useFrameStore(s => s.setColor);
+
+  const skin = useSkinStore(s => s.skin);
+  const setSkin = useSkinStore(s => s.setSkin);
+
   const setStep = useProgressStore(state => state.setStep);
 
   useEffect(() => {
     setStep(3);
   }, [setStep]);
 
-  const handleButtonClick = (colorId: string, index: number) => {
-    setbgColor(colorId);
-    if (swiperRef.current) {
-      swiperRef.current.slideToLoop(index, 300);
+  const handleButtonClick = (type: BtnClickEventType, id: string, index: number) => {
+    if (type === 'color') {
+      setbgColor(id);
+      if (swiperColorRef.current) {
+        swiperColorRef.current.slideToLoop(index, 300);
+      }
+    } else if (type === 'skin') {
+      setSkin(id);
+      if (swiperSkinRef.current) {
+        swiperSkinRef.current.slideToLoop(index, 300);
+      }
     }
   };
 
-  const initialSlide = COLORS.findIndex(c => c.id === bgColor);
+  const colorInitialSlide = COLORS.findIndex(c => c.id === bgColor);
+  const skinInitialSlideSkin = SKINS.findIndex(s => s.id === skin);
 
   return (
     <div className="flex flex-col w-full items-center">
       <div className="flex flex-col gap-5 w-full items-center">
         <div className="flex flex-col gap-2 w-[70%] text-center pt-10">
           <p>프레임 색상</p>
-          <Carousel swiperRef={swiperRef} initialSlide={initialSlide}>
+          <Carousel swiperRef={swiperColorRef} initialSlide={colorInitialSlide}>
             {COLORS.map((c, index) => (
               <SwiperSlide key={c.id}>
                 <div className="flex justify-center items-center">
@@ -42,7 +61,7 @@ export default function FrameEditPage() {
                   >
                     <button
                       className={`w-8 h-8 rounded-full ${c.color}`}
-                      onClick={() => handleButtonClick(c.id, index)}
+                      onClick={() => handleButtonClick('color', c.id, index)}
                     />
                   </div>
                 </div>
@@ -50,9 +69,32 @@ export default function FrameEditPage() {
             ))}
           </Carousel>
         </div>
-        <div className="flex flex-col gap-2 w-full text-center">
-          <p>스티커</p>
-          <div className="grid grid-flow-col gap-4 overflow-auto">{/*  */}</div>
+        <div className="flex flex-col gap-2 w-[70%] text-center">
+          <p>프레임 스킨</p>
+          <Carousel swiperRef={swiperSkinRef} initialSlide={skinInitialSlideSkin}>
+            {SKINS.map((s, index) => (
+              <SwiperSlide key={s.id}>
+                <div className="flex justify-center items-center">
+                  <div
+                    className={`w-12 h-12 bg-white/50 rounded-full border-2 flex justify-center items-center ${skin === s.id ? 'border-black' : 'border-transparent'}`}
+                  >
+                    <button
+                      className={`w-8 h-8 rounded-full`}
+                      onClick={() => handleButtonClick('skin', s.id, index)}
+                    >
+                      <Image
+                        src={s.icon || '/images/icon/default-icon.png'}
+                        alt={s.id}
+                        width={32}
+                        height={32}
+                        className="rounded-full"
+                      />
+                    </button>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Carousel>
         </div>
         <div className="flex flex-col gap-2 items-center">
           <p>결과물</p>
---
+++ types/skins.ts
@@ -0,0 +1,44 @@
+export interface SkinItem {
+  id: string;
+  icon: string;
+  src: string;
+}
+
+export const SKINS: SkinItem[] = [
+  { id: 'none', icon: '', src: '' },
+  {
+    id: 'golden-leopard',
+    icon: '/images/icon/golden-leopard-icon.png',
+    src: '/images/frame-skin/golden-leopard.png',
+  },
+  {
+    id: 'white-leopard',
+    icon: '/images/icon/white-leopard-icon.png',
+    src: '/images/frame-skin/white-leopard.png',
+  },
+  {
+    id: '3',
+    icon: '/images/icon/golden-leopard-icon.png',
+    src: '/images/frame-skin/golden-leopard.avif',
+  },
+  {
+    id: '4',
+    icon: '/images/icon/golden-leopard-icon.png',
+    src: '/images/frame-skin/golden-leopard.avif',
+  },
+  {
+    id: '5',
+    icon: '/images/icon/golden-leopard-icon.png',
+    src: '/images/frame-skin/golden-leopard.avif',
+  },
+  {
+    id: '6',
+    icon: '/images/icon/golden-leopard-icon.png',
+    src: '/images/frame-skin/golden-leopard.avif',
+  },
+  {
+    id: '7',
+    icon: '/images/icon/golden-leopard-icon.png',
+    src: '/images/frame-skin/golden-leopard.avif',
+  },
+];
