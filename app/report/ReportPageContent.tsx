67cd22b6233160e6a6536519e3f445781092bdcb
LeeYunHwan <cyzhqly@gmail.com>
2025-12-14 21:56:55 +0900

    feat: Report 페이지 퍼블리싱 및 이메일 보내기 기능 구현 (#35)

---
+++ app/report/ReportPageContent.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Button from '@/components/common/Button';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+
+export default function ReportPageContent() {
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_3qlulvc', 'template_ohtjm3g', form.current!, {
+        publicKey: '3L-BnQVXLmhUVVHVw',
+      })
+      .then(
+        () => {
+          console.log('성공!');
+        },
+        error => {
+          console.log('실패...', error.text);
+        },
+      );
+  };
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <p className="text-xl font-bold">개선 사항 및 버그 내용을 적어주세요.</p>
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-4">
+        <input
+          type="email"
+          name="email"
+          placeholder="이메일을 입력해주세요."
+          className="border rounded-xl p-4"
+        />
+        <textarea
+          name="message"
+          placeholder="내용을 입력해주세요."
+          required
+          rows={8}
+          maxLength={500}
+          className="border rounded-xl p-4"
+        />
+        <Button type="submit" className="bg-pink-500">
+          이메일 보내기
+        </Button>
+      </form>
+    </div>
+  );
+}
