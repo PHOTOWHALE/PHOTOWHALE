f52fe563a0626b5e4c1177bcb59365a1d8824142
LeeYunHwan <cyzhqly@gmail.com>
2025-12-15 18:08:52 +0900

    feat: Report 페이지에 메타데이터 설정 (#28)

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,5 +1,8 @@
 import ReportPageContent from '@/app/report/ReportPageContent';
 
+export const metadata = {
+  title: '문의하기',
+};
 export default function ReportPage() {
   return <ReportPageContent />;
 }
