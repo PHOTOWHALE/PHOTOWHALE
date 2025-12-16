e6f4214cbbd5ef6b0099e6e8534803b1a9ecf067
LeeYunHwan <cyzhqly@gmail.com>
2025-12-16 19:07:01 +0900

    fix: 각 페이지 별 성격에 맞도록 페이지 명칭 변경 (#28)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,8 @@
 import ContactPageContent from '@/app/contact/ContactPageContent';
 
 export const metadata = {
-  title: '더 알아보기',
+  title: '문의하기',
 };
-
 export default function ContactPage() {
   return <ContactPageContent />;
 }
--- app/report/page.tsx
+++
@@ -1,8 +0,0 @@
-import ReportPageContent from '@/app/report/ReportPageContent';
-
-export const metadata = {
-  title: '문의하기',
-};
-export default function ReportPage() {
-  return <ReportPageContent />;
-}
