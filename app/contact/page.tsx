568383eb4cb50a3f52fc49db2da80b525fe0881f
LeeYunHwan <cyzhqly@gmail.com>
2025-12-16 18:58:54 +0900

    feat: Contact 페이지 메타데이터 설정 (#28)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,9 @@
 import ContactPageContent from '@/app/contact/ContactPageContent';
 
+export const metadata = {
+  title: '더 알아보기',
+};
+
 export default function ContactPage() {
   return <ContactPageContent />;
 }
