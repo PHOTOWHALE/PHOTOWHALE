15af5391e12575ed593bef522827c5f8c7c26e60
LeeYunHwan <cyzhqly@gmail.com>
2025-12-17 19:03:14 +0900

    feat: Input 컴포넌트 구현 (#38)

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -1,5 +1,7 @@
 import { Input as ShadcnInput } from '@/lib/shadcn/components/common/input';
+import { cn } from '@/lib/utils';
 
-export default function Input({ ...props }: React.ComponentProps<typeof ShadcnInput>) {
-  return <ShadcnInput {...props} />;
+export default function Input({ className, ...props }: React.ComponentProps<typeof ShadcnInput>) {
+  const baseStyle = 'border-gray-400 placeholder:text-gray-500 focus-visible:ring-1';
+  return <ShadcnInput {...props} className={cn(baseStyle, className)} />;
 }
