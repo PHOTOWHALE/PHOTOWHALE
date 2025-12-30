963adcfe191811e9e423e7c7ca663794400e2ec4
LeeYunHwan <cyzhqly@gmail.com>
2025-12-31 02:17:37 +0900

    fix: InfiniteRolling import명 수정 (#66)

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/common/Button';
 import Link from 'next/link';
 import { sendGAEvent } from '@next/third-parties/google';
 import { GA_CTA_EVENTS } from '@/constants/ga';
-import RollingTicker from '@/components/common/homePage/InfiniteRolling';
+import InfiniteRolling from '@/components/common/homePage/InfiniteRolling';
 
 export default function Home() {
   const handleStartClick = () => {
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-2">
       <div className="flex flex-col items-center justify-center w-full absolute">
-        <RollingTicker />
+        <InfiniteRolling />
 
         <div className="absolute flex flex-col justify-center items-center gap-4 z-20 text-white">
           <h1 className="font-bold text-4xl">PHOTOWHALE</h1>
