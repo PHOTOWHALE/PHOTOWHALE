f8c182ea27b565e60789f7dbea0bfbaf43322bcd
LeeYunHwan <cyzhqly@gmail.com>
2026-01-02 22:37:36 +0900

    feat: Header 컴포넌트 클릭 시 store 상태 초기화 구현 및 Make 버튼 라우팅 수정 (#83)

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,12 +2,22 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useFrameStore } from '@/stores/useFrameStore';
+import useSkinStore from '@/stores/useSkinStore';
 
 export default function Header() {
   const pathname = usePathname();
 
+  const resetStores = () => {
+    useFrameStore.getState().reset();
+    useSkinStore.getState().reset();
+  };
+
   return (
-    <ul className="flex items-center justify-around h-12 my-4 px-4 w-[70%] gap-4 bg-[#FFFDF8] font-semibold rounded-full shadow-sm text-sm z-20">
+    <ul
+      onClick={resetStores}
+      className="flex items-center justify-around h-12 my-4 px-4 w-[70%] gap-4 bg-[#FFFDF8] font-semibold rounded-full shadow-sm text-sm z-20"
+    >
       <li>
         <Link href="/" className={`${pathname === '/' ? 'text-[#579fe2]' : ''}`}>
           Home
@@ -15,7 +25,7 @@ export default function Header() {
       </li>
       <li>
         <Link
-          href="/frame/view"
+          href="/frame/select"
           className={`${pathname.includes('/frame') ? 'text-[#579fe2]' : ''}`}
         >
           Make
