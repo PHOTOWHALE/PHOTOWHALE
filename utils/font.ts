845031bde796e0bf52508d0f9b4474d4140b40a5
Sonseongoh <nobleser@naver.com>
2026-01-09 18:39:28 +0900

    fix: pretendard static 방식으로 변경(#107)

--- utils/font.ts
+++ utils/font.ts
@@ -1,5 +1,15 @@
 import localFont from 'next/font/local';
 
+export const pretendard = localFont({
+  src: [
+    { path: '../public/fonts/Pretendard-Regular.woff2', weight: '400', style: 'normal' },
+    { path: '../public/fonts/Pretendard-SemiBold.woff2', weight: '600', style: 'normal' },
+  ],
+  variable: '--font-pretendard',
+  display: 'swap',
+  preload: true,
+});
+
 export const digitalix = localFont({
   src: '../public/fonts/digitalix.ttf',
   variable: '--font-digitalix',
