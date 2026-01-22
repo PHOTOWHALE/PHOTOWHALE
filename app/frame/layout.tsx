27f511e510a36e7f612e7e3c3c3d3808766fa412
LeeYunHwan <cyzhqly@gmail.com>
2026-01-22 18:21:05 +0900

    feat: 로고 아이콘 프리로드 코드 추가 (#148)

--- app/frame/layout.tsx
+++ app/frame/layout.tsx
@@ -3,9 +3,14 @@ import StepBar from '@/components/common/StepBar';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      {/* 아이콘 프리로딩 */}
+      <link rel="preload" as="image" href="/images/icon/logo/photo-whale-logo.webp" />
+      <link rel="preload" as="image" href="/images/icon/logo/photo-whale-xmas-logo.webp" />
+
       <div className="w-full flex justify-center">
         <StepBar />
       </div>
+
       <div className="flex-1 flex flex-col w-full items-center justify-center">{children}</div>
     </>
   );
